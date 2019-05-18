b9ce0beee4116c0f901da5c98033ec3e40716100
Dennis Scharfenberg <ds@dreischild.de>
2019-05-18 20:31:15 +0200

    Erste Datei

---
+++ L01Function.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace AbcTest\Learn;
+
+class L01Function
+{
+    public function run()
+    {
+
+        /*function getClassNameWithNamespace($namespace, $className)
+        {
+            if(false) {
+                return null;
+            } else {
+                return sprintf(
+                    '%s---%s',
+                    $namespace,
+                    $className
+                );
+            }
+        }*/
+        /*function getClassNameWithNamespace($namespace, $className): ?string // ? fuer empty accepted
+        {
+            if(false) {
+                return null;
+            } else {
+                return sprintf(
+                    '%s---%s',
+                    $namespace,
+                    $className
+                );
+            }
+        }*/
+
+        /*
+        function getClassNameWithNamespace(string $namespace, string $className): ?string // ? fuer empty accepted
+        {
+            if(false) {
+                return null;
+            } else {
+                return sprintf(
+                    '%s---%s',
+                    $namespace,
+                    $className
+                );
+            }
+        }*/
+
+        function getClassNameWithNamespace(string $namespace, string $className, string $delimitor = '-'): ?string // ? fuer empty accepted
+        {
+            if(false) {
+                return null;
+            } else {
+                return sprintf(
+                    '%s%s%s',
+                    $namespace,
+                    $delimitor,
+                    $className
+                );
+            }
+        }
+
+        $namespace = 'NAMESPACE01';
+        $className = 'CLASSNAME01';
+
+        $fullClassnameWithNamespace = getClassNameWithNamespace ($namespace, $className);
+        echo $fullClassnameWithNamespace;
+
+        /**
+         * .......
+         */
+
+        $namespace = 'NAMESPACE02';
+        $className = 'CLASSNAME02';
+
+        $fullClassnameWithNamespace = getClassNameWithNamespace ($namespace, $className, "/");
+        echo $fullClassnameWithNamespace;
+    }
+}
